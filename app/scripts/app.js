7e868948dfd1acb9d16f62a672c015b92fdb6850
Erlind Dollani <erlind.dollani@comm-it.it>
2015-10-13 12:16:03 +0100

    My first Yeoman Project Working with Directives

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angulardirectivesApp
+ * @description
+ * # angulardirectivesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angulardirectivesApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angulardirectivesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angulardirectivesApp
+ */
+angular.module('angulardirectivesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
