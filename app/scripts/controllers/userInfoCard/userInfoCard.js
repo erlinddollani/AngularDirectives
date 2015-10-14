1538f98b33a6dfd1bfa7bfde4225ab31a4b48e03
Erlind Dollani <erlind.dollani@comm-it.it>
2015-10-14 15:11:50 +0100

    Made some fixes to userInfoCardController and userInfoCardTemplate.html

--- app/scripts/controllers/userInfoCard/userInfoCard.js
+++ app/scripts/controllers/userInfoCard/userInfoCard.js
@@ -2,6 +2,35 @@
 
 angular.module('Google.controllers')
 	.controller('userInfoCardController', function($scope){
+		$scope.users = {
+			user1: {
+				name: 'Senior Leone',
+				address: {
+					street: 'Donika Kastrioti',
+					city: 'Tirana',
+					planet: 'Earth'
+				},
+				friends: [
+					'Lupi',
+					'Ben',
+					'Marko'
+				]
+			},
+			user2: {
+				name: 'Senior Maestro Leone ',
+				address: {
+				street: 'Donika Kastrioti',
+				city: 'Tirana',
+				planet: 'Earth'
+				},
+				friends: [
+					'Lupito',
+					'Ben',
+					'Marko'
+				]
+			}
+		}
+		/*
 		$scope.user = {
 			name: 'Senior Leone',
 			address: {
@@ -14,8 +43,8 @@ angular.module('Google.controllers')
 				'Ben',
 				'Marko'
 			]
-		}
-
+		}*/
+		/*
 		$scope.user2 = {
 			name: 'Senior Maestro Leone ',
 			address: {
@@ -28,7 +57,7 @@ angular.module('Google.controllers')
 				'Ben',
 				'Marko'
 			]
-		}
+		}*/
 
 		$scope.touchButton = function(user) {
 			user.rank = "Knight";
